049c054da7ae1d76703aff1e3664f3f707830798
AlexanderAntov <a.antov91@gmail.com>
2020-10-28 12:58:37 +0200

    Migrate the Home component to functional

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -3,35 +3,27 @@ import { WeatherForecast } from '../WeatherForecast/WeatherForecast.js';
 import { MainTopics } from '../MainTopics/MainTopics.js';
 import { NavigationTilesConst } from './HomeNavigationTilesConst.js';
 
-class Home extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.goToState = (state) => this._goToState(state);
-    }
-
-    render() {
-        return (
-            <div className="home-container">
-                <div className="col-xs-12 content-strip">
-                    {NavigationTilesConst.map(tile =>
-                        <div key={tile.id} className="functional-tile" onClick={() => this.goToState(tile.state)}>
-                            <div className="navigation-item">
-                                <i className={tile.icon}></i>
-                                <span>{tile.title}</span>
-                            </div>
-                        </div>)
-                    }
-                </div>
-                <WeatherForecast />
-                <MainTopics />
-            </div>
-        );
-    }
-
-    _goToState(state) {
+function Home() {
+    const goToState = (state) => {
         window.location.hash = state;
-    }
+    };
+
+    return (
+        <div className="home-container">
+            <div className="col-xs-12 content-strip">
+                {NavigationTilesConst.map(tile =>
+                    <div key={tile.id} className="functional-tile" onClick={() => goToState(tile.state)}>
+                        <div className="navigation-item">
+                            <i className={tile.icon}></i>
+                            <span>{tile.title}</span>
+                        </div>
+                    </div>)
+                }
+            </div>
+            <WeatherForecast />
+            <MainTopics />
+        </div>
+    );
 }
 
 export { Home };
