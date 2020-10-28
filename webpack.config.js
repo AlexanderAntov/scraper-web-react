76955488a0158320e07a901276ebedbde2157e83
AlexanderAntov <a.antov91@gmail.com>
2020-10-28 12:07:30 +0200

    Bump to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
           {
               test: /\.js$/,
               exclude: /node_modules\/(?!(highcharts)\/).*/,
-              loaders: ['babel-loader']
+              use: ['babel-loader']
           }
       ]
   },
